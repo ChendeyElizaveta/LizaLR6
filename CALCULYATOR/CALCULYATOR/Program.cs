28b2693566a8f409a58ac3329c2abf4d7a8f23d1
Lizavetka20.03 <elizavetachendei@gmail.com>
2024-04-06 18:04:46 +0300

    CALCULYATOR

---
+++ CALCULYATOR/CALCULYATOR/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class CALCULYATOR
+{
+    static void Main()
+    {
+        Console.WriteLine("Простой калькулятор");
+
+        Console.Write("Введите первое число: ");
+        double number1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите операцию (+, -, *, /): ");
+        char operation = Convert.ToChar(Console.ReadLine());
+
+        Console.Write("Введите второе число: ");
+        double number2 = Convert.ToDouble(Console.ReadLine());
+
+        double result = 0;
+
+        switch (operation)
+        {
+            case '+':
+                result = number1 + number2;
+                break;
+            case '-':
+                result = number1 - number2;
+                break;
+            case '*':
+                result = number1 * number2;
+                break;
+            case '/':
+                result = number1 / number2;
+                break;
+        }
+
+        Console.WriteLine("Результат: " + result);
+    }
+}
